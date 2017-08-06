c126deb6db936e1b95fd87a1f45b44df55ed3019
Max Malov <m.malov.v@gmail.com>
2017-08-06 20:06:00 +0600

    Create entry point

---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  CharStream: require('./char-stream'),
+  TextEncoder: require('./text-encoder'),
+  AudioEncoder: require('./audio-encoder'),
+  
+  symbols: require('./symbols')
+};
