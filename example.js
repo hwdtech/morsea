595b81b76b4edb52bcb98cb6e47499ff5403748a
Max Malov <m.malov.v@gmail.com>
2017-08-06 18:08:55 +0600

    Create facade for audio encoder

--- example.js
+++ example.js
@@ -1,16 +1,13 @@
 const pump = require('pump');
-const fs = require('fs');
 const { Encoder } = require('lame');
-const AudioThrough = require('audio-through');
 const CharStream = require('./lib/char-stream');
 const TextEncoder = require('./lib/morse-text-encoder');
-const AudioBufferEncoder = require('./lib/audio-buffer-encoder');
+const AudioEncoder = require('./lib/audio-encoder');
 
 pump(
   CharStream.create('sos'),
   TextEncoder.create(),
-  AudioBufferEncoder.create(),
-  new AudioThrough(),
+  AudioEncoder.create(),
   new Encoder({ bitRate: 32 }),
-  fs.createWriteStream('./test.mp3')
+  process.stdout
 );
